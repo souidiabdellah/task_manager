09057344e157cc06f40e5bdd1bd4ccc2a2cf21e1
souidiabdellah <abdellahsouidi24@gmail.com>
2025-04-08 22:49:44 +0100

    Create entity(User,Task)

--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
